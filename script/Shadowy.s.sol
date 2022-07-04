7f3bd6812092cd8fbd977a45bcca96129e6bc999
krouspy <kenji.lau@ledger.fr>
2022-07-04 12:50:20 +0200

    update script + deploy to goerli

--- script/Shadowy.s.sol
+++ script/Shadowy.s.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Shadower.sol";
+import "../src/Repository.sol";
 
-contract ContractScript is Script {
+contract Shadowy is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+
+		Repository repository = new Repository();
+		Shadower shadower = new Shadower(address(repository));
+
+		vm.stopBroadcast();
     }
 }
